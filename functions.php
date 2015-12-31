9255f13b9e39cc436e6e983c5af41c761dc12b0a
Victor P. Unda <llajta9@gmail.com>
2015-12-31 00:56:12 -0800

    file replace

--- functions.php
+++ functions.php
@@ -13,4 +13,24 @@ $args = array(
 	'height'        => 200,
 	'default-image' => get_template_directory_uri() . '/img/wsu.jpg',
 );
-add_theme_support( 'custom-header', $args );	
+add_theme_support( 'custom-header', $args );
+
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
